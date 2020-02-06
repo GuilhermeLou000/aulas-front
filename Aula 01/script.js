c47b28b4b5924997728b68c8942a1f0f6a7f91e1
GuilhermeLou000 <mendesgui000@gmail.com>
2020-02-06 11:25:35 -0300

    Refatorando e utilizando arrow function

--- Aula 01/script.js
+++ Aula 01/script.js
@@ -2,30 +2,90 @@
 // const $nome = document.querySelector("#nomeAluno");
 
 const $calcular = document.getElementById('calcular');
+const calcularMedia = (n1, n2) => (parseInt(n1) + parseInt(n2))/2;
 
-function calcularMedia() {
+const verifivarSituacao = (media) => media >= 5 ? "Aprovado" : "Reprovado"   
+
+const exibirMedia = () => {
     const $nome = document.getElementById('nomeAluno');
-    const $nota1 = document.getElementById('nota1');
-    const $nota2 = document.getElementById('nota2');
+    const nota1 = document.getElementById('nota1').value;
+    const nota2 = document.getElementById('nota2').value;
     const $media = document.getElementById('media');
     const $situacao = document.getElementById('situacao');
 
     console.log($media.value);
-    const media = (parseInt($nota1.value) + parseInt($nota2.value)) / 2;
+    const media = calcularMedia(nota1,nota2);
 
-    if (media >= 5) {
-        $situacao.value = "Aprovado";
-        $situacao.classList.remove("reprovado")
-        $situacao.classList.add("aprovado")
-
-    } else {
-        $situacao.value = "Reprovado!";
-        $situacao.classList.remove("aprovado")
-        $situacao.classList.add("reprovado");
-
-    }
+    $situacao.value = verifivarSituacao(media);
 
     $media.value= media;
+
+
+    // if($media.value > 0 && $media.value < 3){
+    //     $conceito.value = 'E';
+    // } else if ($media.value >= 3 && $media.value < 5) {
+    //     $conceito.value = 'D';
+    // } else if ($media.value >= 5 && $media.value < 8) {
+    //     $conceito.value = 'C';
+    // } else if ($media.value >= 8 && $media.value < 10) {
+    //     $conceito.value = 'B';
+    // } else if ($media.value == 10) {
+    //     $conceito.value = 'A';
+    // } else if ($media.value >= 11 && $media.value < 30){
+    //     $conceito.value = 'S';
+    // } else if ($media.value >= 30 && $media.value < 60){
+    //     $conceito.value = 'SS';
+    // } else if ($media.value >= 60 && $media.value < 90) {
+    //     $conceito.value = 'SSS';
+    // } else if ($media.value >= 90 && $media.value <= 100) {
+    //     $conceito.value = 'SSS+';
+    // }
 }
 
-$calcular.addEventListener('click', calcularMedia);
+
+const calcularConceito = () => {
+    const media = document.getElementById('media').value 
+    const $conceito = document.getElementById('conceito');
+
+    if(media > 0 && media < 3){
+        $conceito.value = 'E';
+    } else if (media >= 3 && media < 5) {
+        $conceito.value = 'D';
+    } else if (media >= 5 && media < 8) {
+        $conceito.value = 'C';
+    } else if (media >= 8 && media < 10) {
+        $conceito.value = 'B';
+    } else if (media == 10) {
+        $conceito.value = 'A';
+    }else if (media >= 11 && media < 30){
+         $conceito.value = 'S';
+    } else if (media >= 30 && media < 60){
+         $conceito.value = 'SS';
+    } else if (media >= 60 && media < 90) {
+          $conceito.value = 'SSS';
+    } else if (media >= 90 && media <= 100) {
+         $conceito.value = 'SSS+';
+    }
+
+}
+
+//função normal
+function calcular(){
+    exibirMedia();
+    calcularConceito();
+}
+
+// //Função anonima
+// const calcular2 = function (){
+//     calcularMedia();
+//     calcularConceito();
+// }
+
+// //Arrow function
+// const calcualr3 = () => {
+//     calcularMedia();
+//     calcularConceito();
+// }
+
+
+$calcular.addEventListener('click',calcular);
